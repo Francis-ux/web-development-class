db29293742a6dc36240fa4b64918d889a3e0c790
Micheal King <kingmicheal744@gmail.com>
2024-01-10 12:56:51 +0100

    first commit

---
+++ javascript/01-variables-data-types/01-console/script.js
@@ -0,0 +1,16 @@
+// Log number
+console.log(100);
+
+// Log string
+console.log('Hello world');
+
+// Log multiple values
+console.log(20, 'Hello', true);
+
+// Log a variable
+const x = 100;
+console.log(x);
+
+// Console error & warning
+console.error('Alert');
+console.warn('Warning');
---
+++ javascript/01-variables-data-types/02-variables/script.js
@@ -0,0 +1,50 @@
+// Ways to declare variable
+// Var , let & const
+
+// var
+// let
+// const
+
+let firstName = 'Destiny';
+const lastName = 'Micheal';
+let age = 30;
+
+// const variable cannot be reassigned
+// let variable can be reassigned
+
+// let class1, class2, class3;
+
+// class1 = 5;
+// class2 = 7;
+// class3 = 0;
+
+// let class1 = 5;
+// let class2 = 7;
+// let class3 = 0;
+
+// Naming convention
+// Only letters,numbers,underscores and dollar sign
+// Can't start with number
+
+let $123 = 60;
+
+console.log($123);
+
+// Objective:
+// Understand the basics of variables in JavaScript.
+
+// Instructions:
+
+//     Create an HTML file (index.html) and a JavaScript file (script.js).
+//     Link the JavaScript file to the HTML file.
+//     Inside the JavaScript file, perform the following tasks.
+
+// Task 1: Variable Declaration and Initialization (5 points)
+
+//     Declare three variables: firstName, age, and favoriteColor.
+//     Initialize firstName with your first name, age with your age, and favoriteColor with a string representing your favorite color.
+
+// Task 2: Variable Types (5 points)
+
+//     Create variables to represent different types: numberVar, stringVar, and undefinedVar.
+//     Assign a value of your choice to each variable, making sure they correspond to their respective types.
