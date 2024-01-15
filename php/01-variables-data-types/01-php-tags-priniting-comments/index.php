e0fbb9afda24b0707b81d514580bc59257fef2be
Micheal King <kingmicheal744@gmail.com>
2024-01-15 05:52:53 +0100

    commit

---
+++ php/01-variables-data-types/01-php-tags-priniting-comments/index.php
@@ -0,0 +1,46 @@
+<?php
+// This is a single line comment
+
+/*
+    This is a multi line comment
+    This is line 2 
+    This is line 3
+*/
+
+echo 'This is an echo statement';
+echo '<br>';
+print 'This is a print statement';
+print '<br>';
+
+// We can echo more than one string at a time by separating them with a comma
+echo 'This is line 1', 'This is line 2';
+echo '<br>';
+
+/*
+    Using the inbuilt php server
+    php -S localhost:8000 
+ 
+    Explanation
+    8000 = port 
+    localhost = domain 
+    php -S = php server
+    
+*/
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Tags Printing Comments</title>
+</head>
+
+<body>
+    <h1>
+        PHP Tags Printing Comments
+    </h1>
+</body>
+
+</html>
