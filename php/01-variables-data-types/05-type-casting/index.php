73a6347b985744e3a57b3ae23573e55e1f7e112a
Micheal King <kingmicheal744@gmail.com>
2024-01-22 07:14:59 +0100

    commit

---
+++ php/01-variables-data-types/05-type-casting/index.php
@@ -0,0 +1,34 @@
+<?php
+$number = 57;
+$number1 = '90A';
+
+// int to string
+$result = (string)$number;
+var_dump($result);
+echo '<br>';
+
+// string to int
+$result = (int)$number1;
+var_dump($result);
+echo '<br>';
+
+// int to boolean
+$result = (bool)$number;
+var_dump($result);
+echo '<br>';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Type Casting</title>
+</head>
+
+<body>
+    <h1>Type Casting.</h1>
+</body>
+
+</html>
