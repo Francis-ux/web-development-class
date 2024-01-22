7cf098bfdcfd3710ea5a4f4e7e2d9f2e866fac10
Micheal King <kingmicheal744@gmail.com>
2024-01-22 07:04:37 +0100

    commit

---
+++ php/01-variables-data-types/04-strings-concatenation/index.php
@@ -0,0 +1,32 @@
+<?php
+// Concatenation Symbol = .
+
+$firstName = 'John';
+
+$lastName = 'Doe';
+
+$fullName = $firstName . ' ' . $lastName;
+
+echo $fullName;
+
+echo '<br>';
+
+// Concatenation assignment
+$fullName .= ' Jr';
+
+echo $fullName;
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>String Concatenation</title>
+</head>
+
+<body>
+    <h1>String Concatenation</h1>
+</body>
+
+</html>
