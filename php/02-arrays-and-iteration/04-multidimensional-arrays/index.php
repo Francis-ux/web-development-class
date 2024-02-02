9c8422fd2b963a33a51e2d020467ed979419ef59
Micheal King <kingmicheal744@gmail.com>
2024-02-02 05:57:03 +0100

    commit

--- php/02-arrays-and-iteration/04-multidimensional-arrays/index.php
+++ php/02-arrays-and-iteration/04-multidimensional-arrays/index.php
@@ -1,3 +1,48 @@
-<?php 
+<?php
+$output = null;
 
-?>
+$fruits = [
+    [
+        ['Apple', 'Red'],
+        ['Orange', 'Pink'],
+        ['Banana', 'Yellow']
+    ]
+];
+
+// Accessing elements
+
+$output = $fruits[0][0][1];
+
+// Multidimensional associative array
+
+$users = [
+    [
+        ['name' => 'John', 'email' => 'john@gmail.com', 'password' => 'secret'],
+        ['name' => 'Mary', 'email' => 'mary@gmail.com', 'password' => 'secret'],
+        ['name' => 'Jane', 'email' => 'jane@gmail.com', 'password' => 'secret'],
+    ]
+];
+
+$output = $users[0][2]['name'];
+
+// Add a new user
+$users[][] = ['name' => 'alex', 'email' => 'alex@gmail.com', 'password' => 'secret'];
+
+
+// Accessing element
+
+$output = $users[1][0]['name'];
+
+// Remove the last user
+// array_pop($users);
+
+// Remove a specific user
+// unset($users[0][1]);
+
+// count users/elements
+$output = count($users[1]);
+
+// print_r($users);
+
+
+echo $output;
