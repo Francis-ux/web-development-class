fc46ee877a450efa2cb6f03fabb2dfbf64e41264
Micheal King <kingmicheal744@gmail.com>
2024-02-07 05:36:35 +0100

    commit

--- php/02-arrays-and-iteration/05-array-challanges/index.php
+++ php/02-arrays-and-iteration/05-array-challanges/index.php
@@ -1,23 +1,62 @@
 <?php
 // Challenge 1
 
-$numbers = [2, 3, 4, 5, 6];
+// $numbers = [2, 3, 4, 5, 6];
 
-$sum = $numbers[0] + $numbers[1] + $numbers[2] + $numbers[3] + $numbers[4];
+// $sum = $numbers[0] + $numbers[1] + $numbers[2] + $numbers[3] + $numbers[4];
 
-$amount = count($numbers);
+// $amount = count($numbers);
 
-echo 'The sum of the ' . $amount . ' numbers is ' . $sum;
+// echo 'The sum of the ' . $amount . ' numbers is ' . $sum;
 
-// Challenge 2
-$colors = ['red', 'blue', 'green', 'yellow'];
+// // Challenge 2
+// $colors = ['red', 'blue', 'green', 'yellow'];
 
-$colors = array_reverse($colors);
+// $colors = array_reverse($colors);
 
-$colors = array_merge($colors, ['purple', 'orange']);
+// $colors = array_merge($colors, ['purple', 'orange']);
 
-array_splice($colors, 1, 0, 'pink');
+// array_splice($colors, 1, 0, 'pink');
 
-array_pop($colors);
+// array_pop($colors);
 
-print_r($colors);
+// print_r($colors);
+
+// Challenge 3
+
+$listings = [
+    [
+        'id' => 1,
+        'job_title' => 'PHP Developer',
+        'company' => 'Meta',
+        'contact_email' => 'support@meta.com',
+        'contact_phone' => '08131949236',
+        'skills' => ['PHP', 'SQL'],
+    ],
+    [
+        'id' => 2,
+        'job_title' => 'Javascript Developer',
+        'company' => 'Mega Mindset',
+        'contact_email' => 'support@megamindset.com',
+        'contact_phone' => '08131949236',
+        'skills' => ['Javascript', 'Node js'],
+    ]
+];
+
+// Create a new listing using the array push function
+$newListing = [
+    'id' => 3,
+    'job_title' => 'Full Stack Developer',
+    'company' => 'Meta',
+    'contact_email' => 'support@meta.com',
+    'contact_phone' => '08131949236',
+    'skills' => ['HTML', 'CSS', 'BOOTSTRAP', 'JAVASCRIPT', 'PHP', 'SQL'],
+];
+
+array_push($listings, $newListing);
+
+// Print out the job_title of the second job listing in the array
+print_r($listings[1]['job_title']);
+
+// Print out the first skill of the third job listing in the array
+print_r($listings[2]['skills'][0]);
